45c422ee8992e2838814e31f00ec829605ea61bb
Ryan Webb <webbryan92@gmail.com>
2018-04-11 20:53:24 -0400

    Brainboom

--- src/pages/default/default.ts
+++ src/pages/default/default.ts
@@ -10,28 +10,32 @@ export class DefaultPage {
   phaseCount:any;
   turn:any;
   stages = STAGES;
-  hideme: any = {};
-  banphase: any = true;
-  pickphase: any = false;
+  pickphase: any = "ban";
+  //banphase: any = "";
   count: any = 0;
   constructor(public navCtrl: NavController) {
 
   }
-  handleClick(stage){
+  /*handleClick(stage){
     stage.hideme = !stage.hideme;
     this.count = this.count+1;
     this.pickBanTurn();
-  }
-  onClick(item) {
-    Object.keys(this.hideme).forEach(h => {
-      this.hideme[h] = false;
-    });
-    this.hideme[item.id] = true;
   }
   pickBanTurn(){
     if(this.count > 2){
       this.banphase = false;
       this.pickphase = true;
+    }
+  }*/
+  handleClick(stage){
+    stage.hideme = !stage.hideme;
+    stage.phase = this.pickphase;
+    this.count = this.count+1;
+    this.pickBanTurn();
+  }
+  pickBanTurn(){
+    if(this.count > 2){
+      this.pickphase = "pick";
     }
   }
   /*public on(index) {
