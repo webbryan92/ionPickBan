9c1a67aa6eb5f1ad6773ce454da24f9997984551
Ryan Webb <webbryan92@gmail.com>
2018-04-19 14:42:02 -0400

    fixed undo button again

--- src/pages/default/default.ts
+++ src/pages/default/default.ts
@@ -7,8 +7,8 @@ import { STAGES } from '../../services/storage';
   templateUrl: 'default.html'
 })
 export class DefaultPage {
-  phaseCount:any;
-  turn:any;
+  phaseCount: any;
+  turn: any;
   stages = STAGES;
   pickphase: any = "ban";
   picknumber: any = "A";
@@ -22,79 +22,102 @@ export class DefaultPage {
   constructor(public navCtrl: NavController) {
 
   }
-  handleClick(stage, index){
-    if(!this.outOfBounds(this.picknumber) && !this.used(stage)){
+  handleClick(stage, index) {
+    if (!this.outOfBounds(this.picknumber) && !this.used(stage)) {
       this.allowUndo = true;
-      this.count = this.count+1;    
-      this.pickBanTurn();    
-      stage.phase = this.pickphase; 
-      if(stage.phase === "ban"){
+      this.count = this.count + 1;
+      this.pickBanTurn();
+      stage.phase = this.pickphase;
+      if (stage.phase === "ban") {
+        this.undo = stage;
         this.removeItem(stage, index);
+        this.undoIndex = index;
       }
-      else{
+      else {        
         stage.pickNum = this.picknumber;
         this.picknumber = this.nextChar(this.picknumber);
         stage.used = true;
-      }    
+        this.undo = stage;
+        this.undoIndex = index;
+      }
       stage.hideme = !stage.hideme;
+      //switch team
+      this.turnIndex = this.turnIndex + 1;
       this.handleTurn();
-    }     
+    }
   }
-  pickBanTurn(){
-    if(this.count > 2){
+  pickBanTurn() {
+    if (this.count > 2) {
       this.pickphase = "pick";
     }
   }
-  removeItem(stage, index){
-    this.undo = stage;
+  removeItem(stage, index) {    
     this.stages.splice(index, 1);
-    this.undoIndex = index;     
   }
-  restoreElement(){
-    if(this.allowUndo){
-      if(this.undo !== 0){
-        this.stages.splice(this.undoIndex, 0, this.undo);
+  restoreElement() {//SHITS FUCKED OUTSIDE BANS LADS
+    if (this.allowUndo) {
+      if (this.pickphase == "ban") {
+        if (this.undo !== 0) {
+          this.stages.splice(this.undoIndex, 0, this.undo);
+        }
+        this.count = this.count - 1;
+        this.allowUndo = false;
+        //set turn back
+        this.turnIndex = this.turnIndex - 1;
+        this.handleTurn();
       }
-      this.count = this.count-1  
-      this.allowUndo = false;
-    }      
+      else {
+        //handle during pick phase
+        //store index of previously selected item        
+        this.picknumber = this.prevChar(this.picknumber);
+        this.undo.pickNum = null;
+        this.undo.used = false;
+        this.undo.hideme = !this.undo.hideme;
+        this.count = this.count - 1;
+        this.allowUndo = false;
+        this.turnIndex = this.turnIndex - 1;
+        this.handleTurn(); //undo turn order
+      }
+    }
   }
-  phase(phase){
-    if(phase === "pick"){
+  phase(phase) {
+    if (phase === "pick") {
       return true;
     }
   }
-  nextChar(c){
+  nextChar(c) {
     return String.fromCharCode(c.charCodeAt(0) + 1);
   }
-  outOfBounds(p){
-    if(p > "D"){
+  prevChar(c) {
+    return String.fromCharCode(c.charCodeAt(0) - 1);
+  }
+  outOfBounds(p) {
+    if (p > "D") {
       return true;
     }
-    else{
+    else {
       return false;
     }
   }
-  reset(){
+  reset() {
     location.reload();
   }
-  used(stage){
-    if(stage.used){
+  used(stage) {
+    if (stage.used) {
       return true;
     }
-    else{
+    else {
       return false;
     }
   }
-  handleTurn(){
-    this.turnIndex = this.turnIndex+1;
-    if(this.turns[this.turnIndex] == "1"){
+  handleTurn() {
+    if (this.turns[this.turnIndex] == "1") {
       this.teamTurn = "1";
       console.log(this.teamTurn);
     }
     else {
       this.teamTurn = "2";
       console.log(this.teamTurn);
-    }    
+    }
   }
 }
