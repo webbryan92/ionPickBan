f809b241b03afa917dffff9d82583f5a55eab01e
Ryan Webb <webbryan92@gmail.com>
2018-04-24 12:35:35 -0400

    added functionality for "repeat" mode

--- src/pages/default/default.ts
+++ src/pages/default/default.ts
@@ -7,6 +7,7 @@ import { STAGES } from '../../services/storage';
   templateUrl: 'default.html'
 })
 export class DefaultPage {
+  category: any;
   phaseCount: any;
   turn: any;
   stages = STAGES;
@@ -19,46 +20,120 @@ export class DefaultPage {
   turnIndex: any = 0;
   teamTurn: any = this.turns[0];
   allowUndo: boolean;
+  
   constructor(public navCtrl: NavController) {
-
+    this.getDefaults();
+  }
+  getDefaults() {
+    if (localStorage.getItem('category') != null) {
+      this.category = localStorage.getItem('category');
+    } else {
+      this.category = 'unique';
+    }
+    /*if (localStorage.getItem('picklimit') != null) {
+        this.picklimit = localStorage.getItem('picklimit');
+    } else {
+        this.picklimit = 2;
+    }
+    if (localStorage.getItem('banlimit') != null) {
+        this.banlimit = localStorage.getItem('banlimit');
+    } else {
+        this.banlimit = 2;
+    }*/
   }
   handleClick(stage, index) {
-    if (!this.outOfBounds(this.picknumber) && !this.used(stage)) {
-      this.allowUndo = true;
-      this.count = this.count + 1;
-      this.pickBanTurn();
-      stage.phase = this.pickphase;
-      if (stage.phase === "ban") {
-        this.undo = stage;
-        this.removeItem(stage, index);
-        this.undoIndex = index;
-      }
-      else {        
-        stage.pickNum = this.picknumber;
-        this.picknumber = this.nextChar(this.picknumber);
-        stage.used = true;
-        this.undo = stage;
-        this.undoIndex = index;
-      }
-      stage.hideme = !stage.hideme;
-      //switch team
-      this.turnIndex = this.turnIndex + 1;
-      this.handleTurn();
+    switch (this.category) {
+      case 'unique':
+        if (!this.outOfBounds(this.picknumber) && !this.used(stage)) {
+          this.allowUndo = true;
+          this.count = this.count + 1;
+          this.pickBanTurn();
+          stage.phase = this.pickphase;
+          if (stage.phase === "ban") {
+            this.undo = stage;
+            this.removeItem(stage, index);
+            this.undoIndex = index;
+          }else {
+            stage.pickNum = this.picknumber;
+            this.picknumber = this.nextChar(this.picknumber);
+            stage.used = true;
+            this.undo = stage;
+            this.undoIndex = index;
+          }
+
+          stage.hideme = !stage.hideme;
+          //switch team
+          this.turnIndex = this.turnIndex + 1;
+          this.handleTurn();
+        }
+        break;
+      case 'teamUnique':
+        if (!this.outOfBounds(this.picknumber) && !this.used(stage)) {
+          this.allowUndo = true;
+          this.count = this.count + 1;
+          this.pickBanTurn();
+          stage.phase = this.pickphase;
+          if (stage.phase === "ban") {
+            this.undo = stage;
+            this.removeItem(stage, index);
+            this.undoIndex = index;
+          }
+          stage.pickNum = this.picknumber;
+          this.picknumber = this.nextChar(this.picknumber);
+          stage.used = true;
+          this.undo = stage;
+          this.undoIndex = index;
+
+          stage.hideme = !stage.hideme;
+          //switch team
+          this.turnIndex = this.turnIndex + 1;
+          this.handleTurn();
+        }
+        break;
+      case 'repeats':
+        if (!this.outOfBounds(this.picknumber)) {
+          this.allowUndo = true;
+          this.count = this.count + 1;
+          this.pickBanTurn();
+          stage.phase = this.pickphase;
+          if (stage.phase === "ban") {
+            this.undo = stage;
+            this.removeItem(stage, index);
+            this.undoIndex = index;
+          } else {
+            if (stage.pickNum == null) {
+              stage.pickNum = this.picknumber;
+              this.picknumber = this.nextChar(this.picknumber);
+              stage.hideme = !stage.hideme;
+            } else {
+              stage.pickNum = stage.pickNum + ' ' + this.picknumber;
+              this.picknumber = this.nextChar(this.picknumber);
+            }
+            stage.used = true;
+            this.undo = stage;
+            this.undoIndex = index;
+          }          
+          //switch team
+          this.turnIndex = this.turnIndex + 1;
+          this.handleTurn();
+        }
+        break;
     }
+
   }
   pickBanTurn() {
     if (this.count > 2) {
       this.pickphase = "pick";
     }
   }
-  removeItem(stage, index) {    
+  removeItem(stage, index) {
     this.stages.splice(index, 1);
   }
-  restoreElement() {//SHITS FUCKED OUTSIDE BANS LADS
+  restoreElement() {
     if (this.allowUndo) {
       if (this.pickphase == "ban") {
         if (this.undo !== 0) {
-          this.stages.splice(this.undoIndex, 0, this.undo);
+          this.stages.splice(this.undoIndex, 0, this.undo);//remove stage from the list
         }
         this.count = this.count - 1;
         this.allowUndo = false;
@@ -67,16 +142,14 @@ export class DefaultPage {
         this.handleTurn();
       }
       else {
-        //handle during pick phase
-        //store index of previously selected item        
-        this.picknumber = this.prevChar(this.picknumber);
-        this.undo.pickNum = null;
-        this.undo.used = false;
-        this.undo.hideme = !this.undo.hideme;
-        this.count = this.count - 1;
-        this.allowUndo = false;
-        this.turnIndex = this.turnIndex - 1;
-        this.handleTurn(); //undo turn order
+        this.picknumber = this.prevChar(this.picknumber); //set pick number back from ex. "B" to "A"
+        this.undo.pickNum = null;                         //take away pickNum given(uncessary?)
+        this.undo.used = false;                           //flag item as unused
+        this.undo.hideme = !this.undo.hideme;             //flip the hidden text property to re-hide
+        this.count = this.count - 1;                      //reduce count of actions for determining pick/ban
+        this.allowUndo = false;                           //Don't allow another undo
+        this.turnIndex = this.turnIndex - 1;              //reset the index of whose turn it is.
+        this.handleTurn();                                //undo turn order
       }
     }
   }
@@ -113,11 +186,9 @@ export class DefaultPage {
   handleTurn() {
     if (this.turns[this.turnIndex] == "1") {
       this.teamTurn = "1";
-      console.log(this.teamTurn);
     }
     else {
       this.teamTurn = "2";
-      console.log(this.teamTurn);
     }
   }
 }
--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -3,37 +3,37 @@ import { NavController } from 'ionic-angular';
 import { DefaultPage } from '../default/default';
 
 @Component({
-  selector: 'settings',
-  templateUrl: 'settings.html'
+    selector: 'settings',
+    templateUrl: 'settings.html'
 })
 export class SettingsPage {
     category: any;
-    picklimit:any;
-    banlimit
-  constructor(public navCtrl: NavController) {
-    this.getDefaults();
-  }
-  getDefaults(){
-      if(localStorage.getItem('category')!= null){
-          this.category = localStorage.getItem('category');
-      } else {
-          this.category = 'unique';
-      }
-      if(localStorage.getItem('picklimit')!= null){
-        this.picklimit = localStorage.getItem('picklimit');
-    } else {
-        this.picklimit = 2;
+    /*picklimit: any;
+    banlimit*/
+    constructor(public navCtrl: NavController) {
+        this.getDefaults();
     }
-    if(localStorage.getItem('banlimit')!= null){
-        this.banlimit = localStorage.getItem('banlimit');
-    } else {
-        this.banlimit = 2;
+    getDefaults() {
+        if (localStorage.getItem('category') != null) {
+            this.category = localStorage.getItem('category');
+        } else {
+            this.category = 'unique';
+        }
+        /*if (localStorage.getItem('picklimit') != null) {
+            this.picklimit = localStorage.getItem('picklimit');
+        } else {
+            this.picklimit = 2;
+        }
+        if (localStorage.getItem('banlimit') != null) {
+            this.banlimit = localStorage.getItem('banlimit');
+        } else {
+            this.banlimit = 2;
+        }*/
     }
-  }
-  setDefaults(){
-      localStorage.setItem('category', this.category);
-      localStorage.setItem('picklimit', this.picklimit);
-      localStorage.setItem('banlimit', this.picklimit);
-      //this.navCtrl.push(RedditsPage);
-  }
+    setDefaults() {
+        localStorage.setItem('category', this.category);
+        //localStorage.setItem('picklimit', this.picklimit);
+        //localStorage.setItem('banlimit', this.picklimit);
+        this.navCtrl.push(DefaultPage);
+    }
 }
